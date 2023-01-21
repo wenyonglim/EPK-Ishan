705e2d3c7c4c49dea112d237f9e2edb6f731bea9
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-21 17:12:37 +0000

    Adding placeholder image

--- epk-website/pages/index.js
+++ epk-website/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
-import profilePic from '../public/Artwork10.JPEG';
+import profilePic from '../public/Artwork1.JPEG';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,14 +17,7 @@ export default function Home() {
       </Head>
       <main>
         <h1>Ishan's EPK</h1>
-        <Image
-          src={profilePic}
-          alt='Picture of the author'
-          width={500}
-          height={500}
-          blurDataURL='data:...'
-          placeholder='blur' // Optional blur-up while loading
-        />
+        <Image src={profilePic} alt='Picture of the author' width={500} />
       </main>
     </>
   );
