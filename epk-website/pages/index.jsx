9c02127ed661964bedc7174f0ada8faf7fd2bd03
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-26 19:10:42 +0000

    Revert "Installed Chakra UI and creating different components"
    This reverts commit c165d1469e6145d56398c2884ce4b2ebd17051c4.

--- epk-website/pages/index.jsx
+++ epk-website/pages/index.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
 import profilePic from '../public/Artwork1.JPEG';
 import Nav from '@/components/Nav';
-import { ChakraProvider } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
 
 export default function Home() {
   return (
@@ -16,6 +16,10 @@ export default function Home() {
       </Head>
       <main>
         <Nav />
+        <h1>Ishan's EPK</h1>
+        <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+          <Image src={profilePic} alt='Picture of Ishan' width={500} />
+        </motion.div>
       </main>
     </>
   );
