54a8aae2c0baa67a5663928fd7eb456c71731b65
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-27 15:23:45 +0000

    Creating custom theme in _app

--- epk-website/pages/_app.js
+++ epk-website/pages/_app.js
@@ -1,5 +1,5 @@
 import '@/styles/globals.css';
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -9,3 +9,24 @@ export default function App({ Component, pageProps }) {
     </NextUIProvider>
   );
 }
+
+// Custom theme and pass  custom theme values
+const theme = createTheme({
+  type: 'dark', // it could be "light" or "dark"
+  theme: {
+    colors: {
+      primary: '#4ADE7B',
+      secondary: '#F9CB80',
+      error: '#FCC5D8',
+    },
+  },
+});
+
+// ^ Pass the new `theme`` to the `NextUIProvider`
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <NextUIProvider theme={theme}>
+//       <Component {...pageProps} />
+//     </NextUIProvider>
+//   )
+// }
