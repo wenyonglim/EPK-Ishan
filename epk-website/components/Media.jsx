4f97e22fe4d0e6a5795f2ffdbc1dd62a9048e81c
Cheng Lim <wenyong.lim@icloud.com>
2023-01-30 23:53:05 +0000

    Overflow and flex positioning for Media component

--- epk-website/components/Media.jsx
+++ epk-website/components/Media.jsx
@@ -27,36 +27,38 @@ export default function Media() {
   ];
 
   return (
-    <Grid.Container gap={2} justify='flex-start'>
-      {list.map((item, index) => (
-        <Grid xs={6} sm={3} key={index}>
-          <Card isPressable>
-            <Card.Body css={{ p: 0 }}>
-              <Card.Image
-                src={'https://nextui.org' + item.img}
-                objectFit='cover'
-                width='100%'
-                height={140}
-                alt={item.title}
-              />
-            </Card.Body>
-            <Card.Footer css={{ justifyItems: 'flex-start' }}>
-              <Row wrap='wrap' justify='space-between' align='center'>
-                <Text b>{item.title}</Text>
-                <Text
-                  css={{
-                    color: '$accents7',
-                    fontWeight: '$semibold',
-                    fontSize: '$sm',
-                  }}
-                >
-                  {item.price}
-                </Text>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Grid>
-      ))}
-    </Grid.Container>
+    <div className='overflow-auto flex justify-center py-8'>
+      <Grid.Container gap={2} justify='flex-start'>
+        {list.map((item, index) => (
+          <Grid xs={6} sm={3} key={index}>
+            <Card isPressable>
+              <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                  src={'https://nextui.org' + item.img}
+                  objectFit='cover'
+                  width='100%'
+                  height={140}
+                  alt={item.title}
+                />
+              </Card.Body>
+              <Card.Footer css={{ justifyItems: 'flex-start' }}>
+                <Row wrap='wrap' justify='space-between' align='center'>
+                  <Text b>{item.title}</Text>
+                  <Text
+                    css={{
+                      color: '$accents7',
+                      fontWeight: '$semibold',
+                      fontSize: '$sm',
+                    }}
+                  >
+                    {item.price}
+                  </Text>
+                </Row>
+              </Card.Footer>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
+    </div>
   );
 }
