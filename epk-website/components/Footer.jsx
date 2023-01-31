62f85c46f4faccdc6f3145a5dbe3cb52631d472b
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-31 12:25:40 +0000

    Removed Links inside footer
    Already inside persistent Top Nav bar

--- epk-website/components/Footer.jsx
+++ epk-website/components/Footer.jsx
@@ -7,30 +7,6 @@ const Footer = () => {
       <div className='items-center grid-flow-col'>
         <p>Copyright © 2023 - All rights reserved</p>
       </div>
-      <div className='grid-flow-col gap-4 md:place-self-center md:justify-self-end'>
-        <Link
-          color='text'
-          target='_blank'
-          href='https://music.apple.com/gb/artist/ishan/1450501527'
-        >
-          <FaApple />
-        </Link>
-        <Link
-          color='text'
-          target='_blank'
-          href='https://open.spotify.com/artist/31H6pWphv3pWESSd03Ifyc?si=ET8qeQCrS3KUhPfSsg9x9A'
-        >
-          <FaSpotify />
-        </Link>
-        <Link
-          color='text'
-          target='_blank'
-          className='pr-2'
-          href='https://youtube.com/@sassyprincesss'
-        >
-          <FaYoutube />
-        </Link>
-      </div>
     </footer>
   );
 };
