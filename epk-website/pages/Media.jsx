2509b0e8f3b39f87adc68678891dcbf4eb6aeb21
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-01-31 22:24:11 +0000

    Placeholder for Media Page

--- epk-website/pages/Media.jsx
+++ epk-website/pages/Media.jsx
@@ -1,9 +1,24 @@
 import Nav from '@/components/Nav';
+import Image from 'next/image';
+import Artwork1 from '../public/Artwork1.jpeg';
+import MediaCard from '@/components/MediaCard';
 
 const Media = () => {
   return (
     <div>
       <Nav Media={'underline'} />
+      <div className='flex justify-center'>
+        <div className='py-8 space-y-5 px-3'>
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+        </div>
+        <div className='py-8 space-y-5 px-3'>
+          <MediaCard />
+          <MediaCard />
+          <MediaCard />
+        </div>
+      </div>
     </div>
   );
 };
