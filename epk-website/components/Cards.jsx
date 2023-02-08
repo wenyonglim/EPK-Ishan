5f5eef21f8109085a177ff125771c14ae578e34c
Cheng Lim <wenyong.lim@icloud.com>
2023-02-08 21:54:19 +0000

    Deleting Cards 1-7

--- epk-website/components/Cards.jsx
+++ epk-website/components/Cards.jsx
@@ -1,12 +1,5 @@
 import { Grid } from '@nextui-org/react';
 import { CardComponent } from './CardComponent';
-import { Card1 } from './Card1';
-import { Card2 } from './Card2';
-import { Card3 } from './Card3';
-import { Card4 } from './Card4';
-import { Card5 } from './Card5';
-import { Card6 } from './Card6';
-import { Card7 } from './Card7';
 
 export default function Cards() {
   return (
