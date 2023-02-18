25f39e0b5bcb941728ebe1fa15c48e4f15075760
PACMAN-3\wenyo <wenyong.lim@icloud.com>
2023-02-18 00:29:01 +0000

    Changing Gallery duration

--- epk-website/components/Gallery.jsx
+++ epk-website/components/Gallery.jsx
@@ -19,6 +19,7 @@ const Gallery = () => {
       className='overflow-hidden text-gray-700'
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
     >
       <div className='container px-5 py-2 mx-auto lg:pt-12 lg:px-32'>
         <div className='flex flex-wrap -m-1 md:-m-2'>
